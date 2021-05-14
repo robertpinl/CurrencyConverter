f684cb1793e9d99d04f8be5e3d8ada48768c43ea
Robert P <robert.pinl@icloud.com>
2021-05-14 15:50:14 +0100

    Change settings menu style

---
+++ CurrencyConverter/Controller/SettingsTableVC.swift
@@ -0,0 +1,91 @@
+//
+//  SettingsVC.swift
+//  CurrencyConverter
+//
+//  Created by Robert Pinl on 21.03.2021.
+//
+
+import UIKit
+
+class SettingsTableVC: UITableViewController, UITextFieldDelegate {
+    
+    let defaults = UserDefaults.standard
+    var ecbRateDiff = 0.0
+    let formatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.maximumFractionDigits = 2
+        nf.minimumFractionDigits = 0
+        return nf
+    }()
+    
+    
+    @IBOutlet weak var ecbDiffTextField: UITextField!
+    @IBOutlet weak var defaultCurrencyButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+                
+        ecbRateDiff = defaults.double(forKey: "ecbDiff")
+        ecbDiffTextField.text = formatter.string(from: NSNumber(value: ecbRateDiff))
+
+        if (defaults.string(forKey: "defaultRate") != nil) {
+        defaultCurrencyButton.setTitle(defaults.string(forKey: "defaultRate"), for: .normal)
+        } else {
+            defaultCurrencyButton.setTitle(CurrencyStore.shared.currencies[46].symbol, for: .normal)
+        }
+    }
+    
+    @IBAction func ecbDiffChanged(_ sender: UITextField) {
+        let value = sender.text!.replacingOccurrences(of: ",", with: ".")
+        guard let doubleValue = Double(value) else { return }
+        ecbRateDiff = doubleValue
+        defaults.set(value, forKey: "ecbDiff")
+        
+        if sender.text == "" {
+            ecbRateDiff = 0.0
+            defaults.set(0.0, forKey: "ecbDiff")
+        }
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.text = ""
+    }
+    
+    //User input validation
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        if string.isEmpty { return true }
+        
+        guard let oldText = textField.text, let r = Range(range, in: oldText) else {
+            return true
+        }
+        
+        let newText = oldText.replacingCharacters(in: r, with: string)
+        
+        let substringToReplace = oldText[r]
+        let numberOfCharacters = oldText.count - substringToReplace.count + string.count
+        
+        return numberOfCharacters <= 5 && newText.isValidDouble(maxDecimalPlaces: 2)
+    }
+    
+    @IBAction func defaultCurrencyButtonPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "GoToDefaultCurrencySelection" , sender: self)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "GoToDefaultCurrencySelection" {
+            let destination = segue.destination as? CurrencySelectionTableVC
+            destination?.delegate = self            
+        }
+    }
+}
+
+//Set default rate currency
+extension SettingsTableVC: CurrencySelectionDelegate {
+    func didSelectCurrency(currency: Currency) {
+        defaultCurrencyButton.setTitle(currency.symbol, for: .normal)
+        defaults.setValue(currency.symbol, forKey: "defaultRate")
+    }
+}
--- CurrencyConverter/Controller/SettingsVC.swift
+++ CurrencyConverter/Controller/SettingsVC.swift
@@ -2,94 +2,16 @@
 //  SettingsVC.swift
 //  CurrencyConverter
 //
-//  Created by Robert Pinl on 21.03.2021.
+//  Created by Robert P on 14.05.2021.
 //
 
 import UIKit
 
-class SettingsVC: UIViewController, UITextFieldDelegate {
-    
-    let defaults = UserDefaults.standard
-    var ecbRateDiff = 0.0
-    let formatter: NumberFormatter = {
-        let nf = NumberFormatter()
-        nf.maximumFractionDigits = 2
-        nf.minimumFractionDigits = 0
-        return nf
-    }()
-    
-    
-    @IBOutlet weak var ecbDiffTextField: UITextField!
-    @IBOutlet weak var defaultCurrencyButton: UIButton!
-    
-    
+class SettingsVC: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
-                
-        ecbRateDiff = defaults.double(forKey: "ecbDiff")
-        ecbDiffTextField.text = formatter.string(from: NSNumber(value: ecbRateDiff))
-        
-        if (defaults.string(forKey: "defaultRate") != nil) {
-        defaultCurrencyButton.setTitle(defaults.string(forKey: "defaultRate"), for: .normal)
-        } else {
-            defaultCurrencyButton.setTitle(CurrencyStore.shared.currencies[46].symbol, for: .normal)
-        }
-    }
-    
-    @IBAction func ecbDiffChanged(_ sender: UITextField) {
-        let value = sender.text!.replacingOccurrences(of: ",", with: ".")
-        guard let doubleValue = Double(value) else { return }
-        ecbRateDiff = doubleValue
-        defaults.set(value, forKey: "ecbDiff")
-        
-        if sender.text == "" {
-            ecbRateDiff = 0.0
-            defaults.set(0.0, forKey: "ecbDiff")
-        }
-    }
-    
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        textField.text = ""
-    }
-    
-    //User input validation
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        
-        if string.isEmpty { return true }
-        
-        guard let oldText = textField.text, let r = Range(range, in: oldText) else {
-            return true
-        }
-        
-        let newText = oldText.replacingCharacters(in: r, with: string)
-        
-        let substringToReplace = oldText[r]
-        let numberOfCharacters = oldText.count - substringToReplace.count + string.count
-        
-        return numberOfCharacters <= 5 && newText.isValidDouble(maxDecimalPlaces: 2)
-    }
-    
-    @IBAction func defaultCurrencyButtonPressed(_ sender: UIButton) {
-        performSegue(withIdentifier: "GoToDefaultCurrencySelection" , sender: self)
-        
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "GoToDefaultCurrencySelection" {
-            let destination = segue.destination as? CurrencySelectionTableVC
-            destination?.delegate = self            
-        }
-    }
-    
-    @IBAction func backgroundPressed(_ sender: UITapGestureRecognizer) {
-        ecbDiffTextField.resignFirstResponder()
-    }
-}
 
-//Set default rate currency
-extension SettingsVC: CurrencySelectionDelegate {
-    func didSelectCurrency(currency: Currency) {
-        defaultCurrencyButton.setTitle(currency.symbol, for: .normal)
-        defaults.setValue(currency.symbol, forKey: "defaultRate")
+        
     }
 }
